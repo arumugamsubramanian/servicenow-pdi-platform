9b4341909ad7608679c098acaaac1a684ca7e84a
Arumugam Subramanian <arumugam1987@gmail.com>
2023-08-30 13:43:16 +0530

    code

--- ServiceNow PDI Platform/src/Server Development/Script Includes/poc1.script.js
+++ ServiceNow PDI Platform/src/Server Development/Script Includes/poc1.script.js
@@ -1,7 +1,9 @@
 var poc1 = Class.create();
 poc1.prototype = {
     initialize: function() {
+        var gr = new GlideRecord('sys_user');
+        gr.get(gs.getUserID());
+        gs.info(gr.name);
     },
-
     type: 'poc1'
 };
